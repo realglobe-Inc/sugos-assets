cc0fea34ab815e61a8417ac1a41921f7606bde63
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-27 18:50:00 +0900

    Add sg favicon

--- test/favicon_test.js
+++ test/favicon_test.js
@@ -12,14 +12,20 @@ describe('favicon', function () {
   this.timeout(3000)
 
   before(() => co(function * () {
-    
+
   }))
 
   after(() => co(function * () {
 
   }))
 
-  it('Favicon', () => co(function * () {
+  it('Generate sugos favicon', () => co(function * () {
+    yield favicon('sugos',
+      `${__dirname}/../tmp/testing-sugos-favicon.svg`
+    )
+  }))
+
+  it('Generate example favicon', () => co(function * () {
     yield favicon('example',
       `${__dirname}/../tmp/testing-example-favicon.svg`,
       {
